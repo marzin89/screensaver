4c10f11be5cc09a95e073e6e0d6fb3a689acdc33
Marco <dead_legend@hotmail.com>
2021-09-02 00:03:09 +0200

    Decrease offset and change position to absolute

--- main.js
+++ main.js
@@ -8,20 +8,22 @@ const body = document.querySelector('body');
 button.addEventListener('click', screensaver);
 
 function screensaver() {
-    let randomWidth = Math.floor(Math.random() * 101);
-    let randomHeight = Math.floor(Math.random() * 101);
-    let randomColor = Math.floor(Math.random() * 12);
-    let randomOffsetLeft = Math.floor(Math.random() * 1900)
-    let randomOffsetTop = Math.floor(Math.random() * 1000);
-    let randomBorderRadius = Math.floor(Math.random() * 101);
+    setInterval(function() {
+        let randomWidth = Math.floor(Math.random() * 101);
+        let randomHeight = Math.floor(Math.random() * 101);
+        let randomColor = Math.floor(Math.random() * 12);
+        let randomOffsetLeft = Math.floor(Math.random() * 1801)
+        let randomOffsetBottom = Math.floor(Math.random() * 901);
+        let randomBorderRadius = Math.floor(Math.random() * 101);
 
-    const div = document.createElement('div');
-    div.style.width = randomWidth + 'px';
-    div.style.height = randomHeight + 'px';
-    div.style.backgroundColor = colors[randomColor];
-    div.style.borderRadius = randomBorderRadius + 'px';
-    div.style.position = 'relative';
-    div.style.left = randomOffsetLeft + 'px';
-    div.style.top = randomOffsetTop + 'px';
-    body.appendChild(div);
+        const div = document.createElement('div');
+        div.style.width = randomWidth + 'px';
+        div.style.height = randomHeight + 'px';
+        div.style.backgroundColor = colors[randomColor];
+        div.style.borderRadius = randomBorderRadius + 'px';
+        div.style.position = 'absolute';
+        div.style.left = randomOffsetLeft + 'px';
+        div.style.top = randomOffsetBottom + 'px';
+        body.appendChild(div);
+    }, 1000);
 }

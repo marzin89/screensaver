8cf58c5244d21d02df795253cf74a1e4aa94cd07
Marco <dead_legend@hotmail.com>
2021-09-01 23:44:41 +0200

    Add a screensaver function
    Add a function that displays elements on the screen when the button is clicked. Set the width, height, border radius, position/offset and color randomly. Select a color from the color array.

--- main.js
+++ main.js
@@ -2,3 +2,26 @@
 
 let colors = ['red', 'blue', 'green', 'pink', 'brown', 'yellow', 'purple', 'orange',
 'aqua', 'navy', 'lime', 'turquoise'];
+
+const button = document.querySelector('button');
+const body = document.querySelector('body');
+button.addEventListener('click', screensaver);
+
+function screensaver() {
+    let randomWidth = Math.floor(Math.random() * 101);
+    let randomHeight = Math.floor(Math.random() * 101);
+    let randomColor = Math.floor(Math.random() * 12);
+    let randomOffsetLeft = Math.floor(Math.random() * 1900)
+    let randomOffsetTop = Math.floor(Math.random() * 1000);
+    let randomBorderRadius = Math.floor(Math.random() * 101);
+
+    const div = document.createElement('div');
+    div.style.width = randomWidth + 'px';
+    div.style.height = randomHeight + 'px';
+    div.style.backgroundColor = colors[randomColor];
+    div.style.borderRadius = randomBorderRadius + 'px';
+    div.style.position = 'relative';
+    div.style.left = randomOffsetLeft + 'px';
+    div.style.top = randomOffsetTop + 'px';
+    body.appendChild(div);
+}

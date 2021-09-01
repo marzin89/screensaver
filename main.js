4341710b0ef47bda5d33a83f9643de8c513a9c15
Marco <dead_legend@hotmail.com>
2021-09-02 00:09:16 +0200

    Add setInterval, run function every second

--- main.js
+++ main.js
@@ -5,25 +5,24 @@ let colors = ['red', 'blue', 'green', 'pink', 'brown', 'yellow', 'purple', 'oran
 
 const button = document.querySelector('button');
 const body = document.querySelector('body');
-button.addEventListener('click', screensaver);
+const run = setInterval(screensaver, 1000);
+button.addEventListener('click', run);
 
 function screensaver() {
-    setInterval(function() {
-        let randomWidth = Math.floor(Math.random() * 101);
-        let randomHeight = Math.floor(Math.random() * 101);
-        let randomColor = Math.floor(Math.random() * 12);
-        let randomOffsetLeft = Math.floor(Math.random() * 1801)
-        let randomOffsetBottom = Math.floor(Math.random() * 901);
-        let randomBorderRadius = Math.floor(Math.random() * 101);
+    let randomWidth = Math.floor(Math.random() * 101);
+    let randomHeight = Math.floor(Math.random() * 101);
+    let randomColor = Math.floor(Math.random() * 12);
+    let randomOffsetLeft = Math.floor(Math.random() * 1801)
+    let randomOffsetBottom = Math.floor(Math.random() * 901);
+    let randomBorderRadius = Math.floor(Math.random() * 101);
 
-        const div = document.createElement('div');
-        div.style.width = randomWidth + 'px';
-        div.style.height = randomHeight + 'px';
-        div.style.backgroundColor = colors[randomColor];
-        div.style.borderRadius = randomBorderRadius + 'px';
-        div.style.position = 'absolute';
-        div.style.left = randomOffsetLeft + 'px';
-        div.style.top = randomOffsetBottom + 'px';
-        body.appendChild(div);
-    }, 1000);
+    const div = document.createElement('div');
+    div.style.width = randomWidth + 'px';
+    div.style.height = randomHeight + 'px';
+    div.style.backgroundColor = colors[randomColor];
+    div.style.borderRadius = randomBorderRadius + 'px';
+    div.style.position = 'absolute';
+    div.style.left = randomOffsetLeft + 'px';
+    div.style.top = randomOffsetBottom + 'px';
+    body.appendChild(div);
 }
